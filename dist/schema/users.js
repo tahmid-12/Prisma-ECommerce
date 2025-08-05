4af643d55b90389a574c630ff9b4428aa3fdb81f
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-08-06 00:26:32 +0600

    Dist has been put is gitignore

--- dist/schema/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SingUpSchema = void 0;
-const zod_1 = require("zod");
-exports.SingUpSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, "Name is required"),
-    email: zod_1.z.string().email("Invalid email format"),
-    password: zod_1.z.string().min(6, "Password must be at least 6 characters long")
-});
